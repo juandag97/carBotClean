7f26b6fbf4bd2f1eb77041b2d207bfdef29ad7ad
juandag97 <juandag@unicauca.edu.co>
2022-05-12 17:18:01 -0500

    Clean code

--- linuxBot.py
+++ linuxBot.py
@@ -1,15 +1,15 @@
 from libraries import * 
 
-class linuxBot(linuxHeadless):
+# class linuxBot(linuxHeadless):
 
-    def __init__(self, URL, DP, DL, BL, UA):
-        linuxHeadless.__init__(self, URL, DP, DL, BL, UA)
+#     def __init__(self, URL, DP, DL, BL, UA):
+#         linuxHeadless.__init__(self, URL, DP, DL, BL, UA)
 
-    def search_car(self, search, file_csv, LINUX_PATH):
-        linuxHeadless.search_car(self, search, file_csv, LINUX_PATH)
+#     def search_car(self, search, file_csv, LINUX_PATH):
+#         linuxHeadless.search_car(self, search, file_csv, LINUX_PATH)
 
-    def tearDown(self):
-        linuxHeadless.tearDown(self)
+#     def tearDown(self):
+#         linuxHeadless.tearDown(self)
 
 def main():
     # file_csv = input("Sobre que archivo quieres escribir los resultados: ")
@@ -20,7 +20,7 @@ def main():
     DL = "/usr/bin/chromedriver"
     BL = "/usr/bin/google-chrome"
     UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
-    car_bot = linuxBot(URL, DP, DL, BL, UA)
+    car_bot = linuxHeadless(URL, DP, DL, BL, UA)
     search_array = (['nissan sentra', 'kia vibro', 'kia sportage', 'mazda 3',
      'toyota corolla', 'volvo', 'audi a4', 'audi a5', 'audi a6', 'volkswagen jetta', 'nissan qashqai', 'bmw serie 3', 'bmw serie 4', 'bmw serie 5', 'bmw serie m'])
     for car in search_array:
--- windowsBot.py
+++ windowsBot.py
@@ -1,16 +1,5 @@
 from libraries import *
 
-class windowsBot(windowsHeadless):
-
-    def __init__(self, URL):
-        windowsHeadless.__init__(self, URL)
-        
-    def search_car(self, search, file_csv, OUT_PATH):
-        windowsHeadless.search_car(self, search, file_csv, OUT_PATH)
-
-    def tearDown(self):
-        windowsHeadless.tearDown(self)
-
 def main():
     file_csv = input("Sobre que archivo quieres escribir los resultados: ")
     OUT_PATH = 'D:\\Actually\\Development\\Cleanbots'
@@ -19,7 +8,7 @@ def main():
     # driver_location = "C:\\Users\\LENOVO\\Downloads\\chromedriver_win32\\chromedriver.exe"
     # binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application"
     # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
-    car_bot = windowsBot(URL)
+    car_bot = windowsHeadless(URL)
     search_array = (['nissan sentra', 'kia vibro', 'kia sportage', 'mazda 3',
      'toyota corolla', 'volvo', 'audi a4', 'audi a5', 'audi a6', 'volkswagen jetta', 'nissan qashqai', 'bmw serie 3', 'bmw serie 4', 'bmw serie 5', 'bmw serie m'])
     for car in search_array:
